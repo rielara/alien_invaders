dd7e153fdbe65bf83f19b6f9a30e804c94365fe0
rielara <78863735+rielara@users.noreply.github.com>
2022-05-11 13:57:46 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -37,8 +37,8 @@ class Settings:
 
     def initialize_dynamic_settings(self):
         #Initialize settings that change throughout the game.
-        self.ship_speed = 1.5
-        self.bullet_speed = 2.0
+        self.ship_speed = 3
+        self.bullet_speed = 4.0
         self.alien_speed = 3.0
 
         # scoring
